3239eaa40c0c54d5a4d3f592cb0662ce5e5ee40d
Thomas MICHELET <thomas@mobilispro.com>
2017-08-10 09:46:07 +0200

    [#1] add bottom bar on places component

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,9 +1,12 @@
 import { LoginComponent } from './components/login/login.component';
+import { PlacesComponent } from './components/places/places.component';
 
 export const routes = [
   { path: '', component: LoginComponent },
+  { path: 'places', component: PlacesComponent },
 ];
 
 export const navigatableComponents = [
-  LoginComponent
+  LoginComponent,
+  PlacesComponent
 ];
