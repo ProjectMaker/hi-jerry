8b0d0179c08fb2ec4ed89e839d93d246e26dca06
Thomas MICHELET <thomas@mobilispro.com>
2017-08-21 13:45:40 +0200

    [#30] structure

---
+++ app/components/places/place-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 'kl-place-detail',
+  templateUrl: 'place-detail.html',
+})
+export class PlaceDetailComponent implements OnInit{
+  public ngOnInit() {}
+}
