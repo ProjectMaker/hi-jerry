9a444198f6e6159bf2bebc4584f6c62553de699f
Thomas MICHELET <thomas@mobilispro.com>
2017-08-21 14:11:17 +0200

    [#30] activity indicator

--- app/components/places/place-detail.component.ts
+++ app/components/places/place-detail.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 
+import { PlaceStorageService } from '../../shared/place/place-storage.service';
+
 @Component({
   moduleId: module.id,
   selector: 'kl-place-detail',
   templateUrl: 'place-detail.html',
 })
 export class PlaceDetailComponent implements OnInit{
-  public ngOnInit() {}
+  public isReady:boolean = false;
+
+  public constructor(private placeStorage:PlaceStorageService) {}
+
+  public ngOnInit() {
+    this.placeStorage.isReady()
+      .subscribe(
+        () => {
+          this.isReady = true;
+        },
+        (err) => console.log(err),
+        () => console.log('completed')
+      );
+  }
 }
