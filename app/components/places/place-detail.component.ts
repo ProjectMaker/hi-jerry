defaca1144df623e230936b4af6ba9390d02ab75
Thomas MICHELET <thomas@mobilispro.com>
2017-08-22 15:23:30 +0200

    [#30] dynamic card detail

--- app/components/places/place-detail.component.ts
+++ app/components/places/place-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute} from '@angular/router';
 
 import { PlaceStorageService } from '../../shared/place/place-storage.service';
 
@@ -9,13 +10,23 @@ import { PlaceStorageService } from '../../shared/place/place-storage.service';
 })
 export class PlaceDetailComponent implements OnInit{
   public isReady:boolean = false;
-
-  public constructor(private placeStorage:PlaceStorageService) {}
+  public place:any = {};
+  public constructor(private placeStorage:PlaceStorageService, private route:ActivatedRoute) {}
 
   public ngOnInit() {
     this.placeStorage.isReady()
       .subscribe(
         () => {
+          this.route.queryParams.subscribe((params:any) => {
+            const id = parseInt(params.id);
+            this.placeStorage.emitter.subscribe(
+              (places) => {
+                this.place = places.find(place => place.id === id);
+              },
+              (err) => console.log(err)
+            );
+            this.placeStorage.fetch();
+          });
           this.isReady = true;
         },
         (err) => console.log(err),
