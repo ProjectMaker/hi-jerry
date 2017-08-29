165a07b0c52a38a41c071e8a856d6b5394faf2f9
Thomas MICHELET <thomas@mobilispro.com>
2017-08-29 12:26:32 +0200

    [#41] use place object for init context & CONTEXT_VALUES for display contexts

--- app/components/places/place.component.ts
+++ app/components/places/place.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CONTEXT_VALUES } from '../../shared/place/place';
 
 @Component({
   moduleId: module.id,
@@ -8,11 +9,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PlaceComponent implements OnInit{
   public iconStar:string = String.fromCharCode(0xf005);
+  public place:any = { note: 0, contexts: ['family'] };
   public note:number = 0;
+  public contextValues:Array<any> = CONTEXT_VALUES;
 
-  public ngOnInit() { }
+  public ngOnInit() {
+    //this.place.note = 0;
+    //this.place.contexts = [];
+  }
 
   public onTapStar(note:number) {
-    this.note = note;
+    this.place.note = note;
+  }
+
+  public onTapContext(context:any) {
+    if (this.place.contexts.indexOf(context.value) === -1) this.place.contexts.push(context.value);
+  }
+
+  public isChecked(context:any) {
+    return this.place.contexts.indexOf(context.value) !== -1;
   }
 }
